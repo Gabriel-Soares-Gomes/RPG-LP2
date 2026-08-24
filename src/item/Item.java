7b42b8ce8f036f48988ac3652d9390de5016d467
soares <gabrielsoaresgomes14@gmail.com>
2026-08-24 17:46:00 -0300

    classe item, enums raridade e tipoitem criados

---
+++ src/item/Item.java
@@ -0,0 +1,34 @@
+package item;
+
+enum Raridade{
+    COMUM,
+    RARO,
+    EPICO,
+    LENDARIO
+}
+
+enum TipoItem{
+
+}
+
+
+public class Item {
+    private String nome;
+    private Raridade raridade;
+    private TipoItem tipo;
+
+
+    public Item(String nome, Raridade raridade, TipoItem tipo){
+        if(nome.isBlank()){
+            throw new IllegalArgumentException("O nome não pode estar em branco!");
+        }
+        
+        this.nome = nome;
+        this.raridade = raridade;
+        this.tipo = tipo;
+    }
+    
+    public String nome() {return nome;}
+    public Raridade raridade() { return raridade; }
+    public TipoItem tipo() {return tipo; }
+}
